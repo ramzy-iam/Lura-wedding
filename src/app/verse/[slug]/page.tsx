15229048312a8f79b1f912f1480f877f4962c65b
Ramsès Feukeng <ramsesfeukeng@gmail.com>
2025-06-11 21:53:13 +0100

    Refactor verse page to use PageProps for parameter handling

--- src/app/verse/[slug]/page.tsx
+++ src/app/verse/[slug]/page.tsx
@@ -1,14 +1,14 @@
-'use client';
 import VerseCard from '@/components/verse/verse-card';
 import { redirect } from 'next/navigation';
 import { VERSES } from './data';
+import { PageProps } from '../../../../.next/types/app/verse/[slug]/page';
 
-export default async function versePage({
-  params,
-}: {
-  params: { slug: string };
-}) {
-  const data = VERSES[params.slug];
+export default async function versePage(
+  { params }: PageProps,
+  // props: Promise<{ params: { slug: string } }>,
+) {
+  const { slug } = await params;
+  const data = VERSES[slug];
   if (!data) {
     redirect('/');
   }
