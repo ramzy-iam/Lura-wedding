08a5ff503a8921aee0f8ba0130bf320944a39e8d
Ramsès Feukeng <ramsesfeukeng@gmail.com>
2025-06-13 22:28:42 +0100

    fix: update text and comment out button in verse page

--- src/app/verse/page.tsx
+++ src/app/verse/page.tsx
@@ -33,7 +33,7 @@ export default function VersePage() {
       />
 
       <p className="mt-11 mb-6 text-lg font-medium text-[#AD1061]">
-        {"Une pensée d'amour 🌷"}
+        {"Une pensée d'amour pour vous 🌷"}
       </p>
 
       <div className="relative z-10 w-full max-w-2xl rounded-2xl bg-gradient-to-br from-[#214f61] via-[#01B7CC] to-[#015d82] p-[6px]">
@@ -51,17 +51,17 @@ export default function VersePage() {
       </div>
 
       <div className="mt-6 flex flex-wrap justify-center gap-4">
-        <button
+        {/* <button
           onClick={regenerate}
           className="rounded-xl bg-[#015d82] px-6 py-2 text-white transition hover:bg-[#005F7C]"
         >
           ✨ Encore une !
-        </button>
+        </button> */}
         <button
           onClick={goHome}
           className="cursor-pointer rounded-xl border border-[#AD1061] px-6 py-2 text-[#AD1061] transition hover:bg-[#AD1061] hover:text-white"
         >
-          {"🌸 Jardin d'Éden"}
+          {"🌸 Allez au Jardin d'Éden"}
         </button>
       </div>
     </main>
